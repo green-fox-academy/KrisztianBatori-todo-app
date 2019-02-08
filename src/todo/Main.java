4fb2c80f5f31e87f3097dbf4d83705f681363181
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-08 11:30:47 +0100

    List tasks

--- src/todo/Main.java
+++ src/todo/Main.java
@@ -1,9 +1,29 @@
 package todo;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         if (args.length != 0) {
-            // Do stuff
+
+            ArrayList<String> todos = new ArrayList<>();
+
+            try {
+                todos = (ArrayList<String>)Files.readAllLines(Paths.get("../KrisztianBatori-todo-app/src/tasks.txt"));
+            } catch (Exception e) {
+                System.out.println("ToDo list can't be opened! 😱");
+            }
+
+            switch (args[0]) {
+                case "-l":
+                    for (int i = 0; i < todos.size(); i++) {
+                        System.out.println((i + 1) + " - " + todos.get(i));
+                    }
+            }
+
         }
         else {
             System.out.println("Command Line Todo application");
