53ac24ad3a5a2f154e95bfdf8c06a05f38c6c64b
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-08 11:55:25 +0100

    Add new task & Add new task error handling

--- src/todo/Main.java
+++ src/todo/Main.java
@@ -3,6 +3,7 @@ package todo;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class Main {
@@ -10,9 +11,10 @@ public class Main {
         if (args.length != 0) {
 
             ArrayList<String> todos = new ArrayList<>();
+            String filePath = "../KrisztianBatori-todo-app/src/tasks.txt";
 
             try {
-                todos = (ArrayList<String>)Files.readAllLines(Paths.get("../KrisztianBatori-todo-app/src/tasks.txt"));
+                todos = (ArrayList<String>)Files.readAllLines(Paths.get(filePath));
             } catch (Exception e) {
                 System.out.println("ToDo list can't be opened! 😱");
             }
@@ -27,6 +29,20 @@ public class Main {
                             System.out.println((i + 1) + " - " + todos.get(i));
                         }
                     }
+                    break;
+                case "-a":
+                    if (args.length < 2 || args[1].charAt(0) == ' ') {
+                        System.out.println("Unable to add: no task provided");
+                    }
+                    else {
+                        todos.add(args[1]);
+                        try {
+                            Files.write(Paths.get(filePath), todos);
+                        } catch (Exception e) {
+                            System.out.println("Couldn't add new task! 😱");
+                        }
+                    }
+                    break;
             }
 
         }
